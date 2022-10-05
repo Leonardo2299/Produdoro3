30d68831f1a2a92ab884e40f88ee14753c899375
Daniel Antunes <danielantunesribeiro15@gmail.com>
2022-10-04 23:05:34 -0300

    implementa classe que edita tarefa

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaModificadaRequest.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaModificadaRequest.java
@@ -1,5 +1,16 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
-public class TarefaModificadaRequest {
+import javax.validation.constraints.NotEmpty;
 
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusAtivacaoTarefa;
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusTarefa;
+import lombok.Getter;
+
+@Getter
+public class TarefaModificadaRequest {
+	@NotEmpty(message = "O campo não pode estar vazio")
+	private String descricao;
+	private StatusTarefa status;
+	private StatusAtivacaoTarefa statusAtivacao;
+	private int contagemPomodoro;
 }
