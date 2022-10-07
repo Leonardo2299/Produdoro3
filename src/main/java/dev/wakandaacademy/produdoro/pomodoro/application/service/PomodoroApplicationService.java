2d8aead282df89232fdd45a5f1797b6ef793a3b4
Léo <leonardo2299@gmail.com>
2022-10-07 18:19:57 -0300

    Review1 - retirada de dependências que não foram usadas e linhas em branco

--- src/main/java/dev/wakandaacademy/produdoro/pomodoro/application/service/PomodoroApplicationService.java
+++ src/main/java/dev/wakandaacademy/produdoro/pomodoro/application/service/PomodoroApplicationService.java
@@ -1,9 +1,6 @@
 package dev.wakandaacademy.produdoro.pomodoro.application.service;
 
-import java.util.UUID;
-
 import dev.wakandaacademy.produdoro.pomodoro.domain.ConfiguracaoPadrao;
-import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 
 public interface PomodoroApplicationService {
 	ConfiguracaoPadrao getConfiguracaoPadrao();
