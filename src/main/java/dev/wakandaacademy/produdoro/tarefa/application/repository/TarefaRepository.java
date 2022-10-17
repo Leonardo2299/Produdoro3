94b9e71e30ba847cae9944e0dd70afd6a1d61f27
rodrigo183 <digo.marqualves@gmail.com>
2022-10-17 14:49:29 -0300

    Implementa fluxo repository

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/repository/TarefaRepository.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/repository/TarefaRepository.java
@@ -2,7 +2,10 @@ package dev.wakandaacademy.produdoro.tarefa.application.repository;
 
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 
-public interface TarefaRepository {
+import java.util.List;
+import java.util.UUID;
 
+public interface TarefaRepository {
     Tarefa salva(Tarefa tarefa);
+    List<Tarefa> buscaTarefaPorIdUsuario(UUID idUsuario);
 }
