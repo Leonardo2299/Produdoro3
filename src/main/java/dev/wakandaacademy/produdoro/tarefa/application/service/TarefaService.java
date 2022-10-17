44ba0b0319bd29aac42b0bb204e7e34997f3b0b7
rodrigo183 <digo.marqualves@gmail.com>
2022-10-17 14:53:51 -0300

    Implementa fluxo Service

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
@@ -2,8 +2,12 @@ package dev.wakandaacademy.produdoro.tarefa.application.service;
 
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
+import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TarefaService {
-
     TarefaIdResponse criaNovaTarefa(TarefaRequest tarefaRequest);
+    List<Tarefa> buscaTarefasPorIdUsuario(Optional<String> idUsuario);
 }
