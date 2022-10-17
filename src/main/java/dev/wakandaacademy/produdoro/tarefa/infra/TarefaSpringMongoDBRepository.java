8e7716be9e7f7527839e684360155aac66ae803e
rodrigo183 <digo.marqualves@gmail.com>
2022-10-17 14:54:46 -0300

    Implementa fluxo repository

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaSpringMongoDBRepository.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaSpringMongoDBRepository.java
@@ -3,7 +3,9 @@ package dev.wakandaacademy.produdoro.tarefa.infra;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TarefaSpringMongoDBRepository extends MongoRepository<Tarefa, UUID> {
+    List<Tarefa> findAllByIdUsuario(UUID idUsuario);
 }
